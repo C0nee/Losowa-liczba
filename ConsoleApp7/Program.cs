3e3162e85cd0142ec62dcee5b6733491bcaa1385
jakub <jakub@DESKTOP-M4FN65S>
2022-03-10 12:14:58 +0100

    Dodaj pliki projektów.

---
+++ ConsoleApp7/Program.cs
@@ -0,0 +1,33 @@
+﻿Random r = new Random();
+int rand = r.Next(0, 10);
+double a;
+Console.WriteLine("zgadnij liczbe");
+a = double.Parse(Console.ReadLine());
+if (a < rand)
+{
+    Console.WriteLine("Podana liczba jest mniejsza od wylosowanej");
+}
+else if (a > rand)
+{
+    Console.WriteLine("Podana liczba jest wieksza od wylosowanej");
+
+}
+else
+    Console.WriteLine("Brawo Trafiles");
+while(a != rand)
+{
+    Console.WriteLine("zgadnij liczbe");
+    a = double.Parse(Console.ReadLine());
+    if (a < rand)
+    {
+        Console.WriteLine("Podana liczba jest mniejsza od wylosowanej");
+    }
+    else if (a > rand)
+    {
+        Console.WriteLine("Podana liczba jest wieksza od wylosowanej");
+
+    }
+    else
+        Console.WriteLine("Brawo Trafiles");
+}
+    
